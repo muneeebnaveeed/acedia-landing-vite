12cd19eb34e226370ee19e9acde71c49593a20f3
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2024-11-02 16:06:35 +0100

    fixed clash grotesk; worked on contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,ts}', './src/*.{html,ts}'],
+  content: ['./src/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
@@ -10,4 +10,7 @@ export default {
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
