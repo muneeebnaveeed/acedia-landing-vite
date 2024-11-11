35c8c0db6ea4ef25d9240920d52c7a9d3a72a8e9
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2024-11-11 14:10:37 +0100

    refactored navbar UI

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,34 @@ function toggleNavbar(navbar: 'desktop' | 'mobile'): void {
   document.body.dataset.navbar = navbar;
 }
 
+function highlightCurrentNavItem(): void {
+  const currentPath = window.location.pathname;
+  const currentHash = window.location.hash;
+  const links = document.querySelectorAll('a.mobile-navbar-item');
+
+  links.forEach((link) => {
+    if (!(link instanceof HTMLAnchorElement)) throw new Error('Invalid mobile navbar menu item');
+
+    const linkPath = link.pathname;
+    const linkHash = link.hash;
+
+    const isHighlighted = linkPath === currentPath && linkHash === currentHash;
+
+    link.dataset.highlight = isHighlighted ? 'true' : 'false';
+  });
+}
+
+function updateCopyrightDate(): void {
+  const htmlElement = document.getElementById('auto-date');
+  if (!htmlElement) throw new Error('Copyright date not found');
+  htmlElement.innerHTML = new Date().getFullYear().toString();
+}
+
+function main(): void {
+  highlightCurrentNavItem();
+  updateCopyrightDate();
+}
+
+main();
+
 window.toggleNavbar = toggleNavbar;
